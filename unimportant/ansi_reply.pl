bd4891685f5c104b20932459484d0bb76083b45a
Dee Newcum <dee.newcum@gmail.com>
2012-06-06 12:02:05 -0500

    cleanup a bit

--- unimportant/ansi_reply.pl
+++ unimportant/ansi_reply.pl
@@ -6,13 +6,12 @@
     use strict;
     use warnings;
 
-    use Time::HiRes qw[time];
+    use Time::HiRes qw[alarm];
     #use Term::ReadKey;
 
     use Data::Dumper;
-    #use Devel::Comments;           # uncomment this during development to enable the ### debugging statements
 
-    use constant TIMEOUT => 2.0;        # total seconds to wait for a reply
+    use constant TIMEOUT => 1.0;        # total seconds to wait for a reply
 
 
 @ARGV or die "$0 <query> [<response_end_character>]\n";
@@ -26,45 +25,22 @@ $query = perl_string_decode($query);
 #Term::ReadKey::ReadMode(2);
 system "stty", '-icanon', '-echo', 'eol', "\001";
 
-my $start_time = time();
+
 print $query;
 $|++;
-#xxd($query);
 
-#print perl_string_encode($query), "\n";
 
-my ($rin, $rout, $input) = ('', '', '');
-vec($rin, fileno(STDIN), 1) = 1;
-
-FINISHED_INPUTTING:
- while (0) {
-    # wait for a key to be pressed
-    my $numchars = 0;
-    while (1) {
-        #print length($input);
-        print ".";
-        last if $numchars = select($rout=$rin, undef, undef, 0.5);
-        last FINISHED_INPUTTING  if (time() - $start_time > TIMEOUT);
-    }
-    #print "\n$numchars char(s) in buffer\n";
-    #for (1..$numchars) {
-    #$input .= getc();
-    while (my $c = getc()) {
-        $input .= $c;
-        last FINISHED_INPUTTING  if (defined($response_end_character) && substr($input, -1) eq $response_end_character);
-    }
-}
-
+my $reply = '';
 eval {
     local $SIG{ALRM} = sub { die "alarm\n" };
     alarm(TIMEOUT);
     while (1) {
         if (defined(my $c = getc())) {
             #print ".";
-            $input .= $c;
-            last if (defined($response_end_character) && $c eq $response_end_character);
+            $reply .= $c;
+            last if (defined($response_end_character)
+                  && $c eq $response_end_character);
         }
-        #last if (time() - $start_time > TIMEOUT);
     }
 };
 if ($@) {
@@ -72,16 +48,9 @@ if ($@) {
 }
 
 
-print "     query:  ", perl_string_encode($query), "\n";
-print "  response:  ", perl_string_encode($input), "\n";
+print "  query:  ", perl_string_encode($query), "\n";
+print "  reply:  ", perl_string_encode($reply), "\n";
 
-
-if (0) {
-    print "\n\n<press enter>\n";
-    my $residual = <STDIN>;
-    $residual =~ s/\n$//s;
-    print "  residual:  >>", perl_string_encode($residual), "<<\n";
-}
 
 ## reset tty mode before exiting
 #Term::ReadKey::ReadMode(0);         
@@ -91,8 +60,7 @@ system 'stty', 'icanon', 'echo', 'eol', chr(0);
 
 
 
-# turn things like "\e" and "\033" into their single-character equivalents, using the normal perl
-# string-literal rules
+# turn things like "\e" and "\033" into their single-character equivalents, using standard perl string-literal rules
 sub perl_string_decode {
     my $encoded = shift;
     my $decoded = eval "qq\000$encoded\000";
